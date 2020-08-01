1ba497dad778211151e806db050187c53982a46c
Andrea Tchalaou <attchalaou@gmail.com>
2020-08-01 06:35:26 +0000

    Automatically backed up by Learn

---
+++ bin/musiclibrary.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+#require_relative '../config/environment'
+
+#MusicLibraryController.new.call
+
+require"./lib/environment.rb"
+
+binding.pry 
+
+puts Concers::Findable("Hello")
